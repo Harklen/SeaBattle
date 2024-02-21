c6401d014602f3f478cfc0078dae96309590aefb
Harklen <as.harklen@gmail.com>
2024-02-21 13:44:07 +0200

    login+register

--- src/main/java/seabattle/app/SecurityConfig.java
+++ src/main/java/seabattle/app/SecurityConfig.java
@@ -32,7 +32,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Конфігурація для вбудованого користувача (замініть це на реальну автентифікацію користувача)
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password(passwordEncoder().encode("password"))
